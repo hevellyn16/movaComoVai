a96b206442c006996693c9d0269943302ec6f669
Gildesio-af <gildesioaf@alu.ufc.br>
2026-06-03 13:02:19 -0300

    chore: initialize back-end project

---
+++ back/mova/src/main/java/com/eng/software/mova/MovaApplication.java
@@ -0,0 +1,13 @@
+package com.eng.software.mova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovaApplication.class, args);
+	}
+
+}
