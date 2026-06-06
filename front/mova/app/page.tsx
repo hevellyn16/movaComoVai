8e6a04fa87fb1f21a5174869bd1835309cc16247
Christian <christianximenessq@gmail.com>
2026-06-06 12:59:41 -0300

    feat: Creation of Register, 404 NotFound and ForgotPassword pages

--- front/mova/app/page.tsx
+++ front/mova/app/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import Header from "../components/layout/header";
 
 export default function Home() {
@@ -7,24 +6,5 @@ export default function Home() {
       <Header />
     </main>
   );
-=======
-// import { redirect } from "next/navigation";
 
-import { AuthLoginPage } from "./auth/login/pageLogin";
-/*
-  export const metadata = {
-  title: "Home",
-};
-
-export default function Home() {
-  redirect("/feed");
-}
-*/
-export default function Home() {
-  return(
-    <main>
-      <AuthLoginPage />
-    </main>
-  )
->>>>>>> Stashed changes
 }
