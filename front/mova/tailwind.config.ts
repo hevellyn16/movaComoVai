a635eb51500187bb18a2af1f2c8e146ee1e22657
Hevellyn <meirianehev@gmail.com>
2026-06-06 10:09:43 -0300

    feat: add button component and update layout

---
+++ front/mova/tailwind.config.ts
@@ -0,0 +1,16 @@
+const config = {
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["var(--font-be-vietnam-pro)", "sans-serif"],
+			},
+			colors: {
+				primary: "#B51B0E",
+				secondary: "#FEAE2C",
+				terciary: "#FFFFFF",
+				quaternary: "#1A1C1E",
+				quinary: "6B4500",
+			},
+		},
+	},
+};
