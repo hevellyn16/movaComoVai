846a40ee1031080b4abfc647622dc5d50f366b69
Christian <christianximenessq@gmail.com>
2026-06-06 11:49:04 -0300

    feat: Implementação da página de login

--- front/mova/app/page.tsx
+++ front/mova/app/page.tsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import Header from "../components/layout/header";
 
 export default function Home() {
@@ -6,4 +7,24 @@ export default function Home() {
       <Header />
     </main>
   );
+=======
+// import { redirect } from "next/navigation";
+
+import { AuthLoginPage } from "./auth/login/pageLogin";
+/*
+  export const metadata = {
+  title: "Home",
+};
+
+export default function Home() {
+  redirect("/feed");
+}
+*/
+export default function Home() {
+  return(
+    <main>
+      <AuthLoginPage />
+    </main>
+  )
+>>>>>>> Stashed changes
 }
